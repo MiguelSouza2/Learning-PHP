b0268344f97bd8464f456e1beaa225fd59557f4a
miguel_casa <130250696+MiguelSouza2@users.noreply.github.com>
2024-04-17 19:55:10 -0300

    PHP-3-- LOGIN

---
+++ PHP_3/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP-3</title>
+</head>
+
+<body>
+    <form action="login.php" method="post">
+        <div>
+            <label for="username">Username</label>
+            <input type="text" name="username">
+            <label for="email">email</label>
+            <input type="text" name="email">
+            <label for="password">password</label>
+            <input type="password" name="password">
+            <button>submit</button>
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ PHP_3/login.php
@@ -0,0 +1,27 @@
+<?php
+
+
+//Criando a sessão
+session_start();
+
+
+//Validação dos caracteres
+if (isset($_POST['username']) && isset($_POST['email']) && isset($_POST['password'])) {
+    function validation($data){
+        $data = trim($data);
+        $data = stripslashes($data);
+        $data = htmlspecialchars($data);
+        return $data;
+    }
+}
+
+//Definindo as variáveis
+$username = validation($_POST['username']);
+$password = validation($_POST['password']);
+$email = validation($_POST['email']);
+
+
+//Se estiver vazio, retorne o erro
+if(empty(trim($username)) || empty(trim($password)) || empty(trim($email))){
+    header('Location: index.php?error=there is an empty input');
+}
